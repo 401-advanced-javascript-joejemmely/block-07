4f2146e7946bc08ec21de1c2416d2f305a994cf9
Joé Jemmely <joe.jemmely@gmail.com>
2019-07-01 09:32:13 -0700

    v1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
+/** @jsx jsx */
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { css, jsx, Global } from '@emotion/core';
+
+import Footer from './components/styles/Footer';
+import Header from './components/styles/Header';
+import RESTy from './components/RESTy';
+import reset from './lib/reset.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      css={css`
+        height: 100%;
+        display: flex;
+        flex-flow: column nowrap;
+      `}
+    >
+      <Global
+        styles={css`
+          ${reset}
+          html {
+            height: 100%;
+          }
+
+          body {
+            background: #eee;
+            height: 100%;
+            flex-direction: column;
+            font-family: Verdana, Geneva, Tahoma, sans-serif;
+          }
+
+          #root {
+            height: 100%;
+          }
+
+          h2 {
+            font-weight: 700;
+            margin-bottom: 0.5em;
+          }
+        `}
+      />
+      <Header>
+        <h1>RESTy</h1>
+      </Header>
+      <RESTy />
+      <Footer>
+        <p>©2019 Code Fellows</p>
+      </Footer>
     </div>
   );
 }
